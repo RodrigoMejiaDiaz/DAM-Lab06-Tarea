8b7f73f46edf62da10407ebffcf03c1a29e8f6c9
RodrigoMejiaDiaz <rodrigoemdiaz@hotmail.com>
2021-04-29 22:56:03 -0500

    bar, icons, components video/settings

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import LaLista from './components/LaLista';
 import Details from './components/Details';
 import Login from './components/Login';
+import Settings from './components/Settings';
+import Video from './components/Video';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+// const Tab = createMaterialBottomTabNavigator();
 
 const App = () => {
   const LogoTitle2 = () => {
@@ -17,6 +23,49 @@ const App = () => {
       </View>
     );
   };
+  function ListaStack() {
+    return (
+      <Tab.Navigator
+        tabBarOptions={{
+          inactiveBackgroundColor: '#f2f2f2',
+        }}>
+        <Tab.Screen
+          name="Listado"
+          component={LaLista}
+          options={{
+            tabBarLabel: 'Listado',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="text-box"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Video"
+          component={Video}
+          options={{
+            tabBarLabel: 'Video',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="video" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarLabel: 'Ajustes',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="cog" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -24,7 +73,7 @@ const App = () => {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen
           name="Ofertas"
-          component={LaLista}
+          component={ListaStack}
           options={{
             detachPreviousScreen: true,
             header: props => <LogoTitle2 {...props} />,
